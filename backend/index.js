8b336061b06b2effcb966de7b8cd421218a41d2b
michaelleonard15 <47129066+michaelleonard15@users.noreply.github.com>
2019-10-06 10:11:48 -0700

    Somewhat working 3-tier with node/express

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const cors = require('cors')
+const mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'cs157a'
+})
+
+connection.connect()
+app.use(cors())
+
+const QRY = 'SELECT * FROM emp;'
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/react-test', (req, res) => res.send('Hi React, I\'m express.'))
+app.get('/mysql-test', (req, res) => {
+    connection.query(QRY, (err, rows, fields) => {
+      console.log(err)
+      console.log(rows)
+      console.log(fields)
+      if(err) throw err
+      res.send(rows)
+    })
+  })
+
+app.get('/full-test', (req, res) => {
+  connection.query(QRY, (err, rows, fields) => {
+    if(err) {console.log(err)}
+    res.send(rows)
+  })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,38 +5,54 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {data: []};
+    this.state = {data: "Nothing yet", rows: []};
   }
 
 
   getData() {
-    let url = "http://127.0.0.1:8080/test/api/test"
+    let url = "http://127.0.0.1:5000/react-test"
     let fetchMethod = { method: 'GET', mode: 'cors'}
-    fetch(url, fetchMethod).then( (response) => {
-      if(response.ok) {
-        console.log(response)
-        this.setState({data: response.data})
-      }
-    } ).catch( (err) => {
-      console.log("The fetch operation failed")
+
+    let text
+
+    fetch(url, fetchMethod).then(res => {
+      res.text().then( theText => {
+        console.log(theText)
+        text = theText
+        console.log("After assignment")
+        console.log(text)
+        this.setState({data: text})  
+      })
     })
-
-
-    if(this.state.data.length === 0) {
-      return "Fail"
-    }
-    else {
-      return (
-        <table>
-          {this.state.data.map( (row) => {
-            return (
-              <tr><td>{row}</td></tr>
-            )}
-          )}
-        </table>
-    }
   }
 
+
+  testFull() {
+    let url = "http://127.0.0.1:5000/full-test"
+    let fetchMethod = { method: 'GET', mode: 'cors'}
+
+    fetch(url, fetchMethod).then(res => {
+      res.json().then( (json) => {
+        this.setState({rows: json})
+      })
+    })
+  }
+
+
+
+  displayRows() {
+    return (
+      <table><tbody>
+        {this.state.rows.map( (row, index) => {
+          return (<tr key={index}>
+                    <td>{row.age}</td>
+                    <td>{row.id}</td>
+                    <td>{row.name}</td>
+                  </tr>)
+          })
+        }
+      </tbody></table>)
+  }
 
   render() {
     return (
@@ -44,7 +60,10 @@ class App extends React.Component {
         <header className="App-header"></header>
         <button onClick={this.getData.bind(this)}>Click Me!</button>
         <br/>
-        {this.getData()}
+        <h1>{this.state.data}</h1>
+        <br/>
+        <button onClick={this.testFull.bind(this)}>Now Click Me!</button>
+        {this.displayRows()}
       </div>
     );
   }
